5bf2bd0ef8edadadb5181a9ed2aa2bd15e7de268
taka-yoko <*****@users.noreply.github.com>
2020-03-16 23:52:03 +0900

    finished ch2_2

---
+++ ch2_2.sql
@@ -0,0 +1,52 @@
+-- ex
+select
+  ProductName
+, Price
+from
+  Products
+;
+
+-- Q1
+select
+  CustomerName
+, Address
+from
+  Customers
+;
+
+-- Q2
+select
+  ProductID
+, ProductName
+, Price
+from
+  Products
+;
+
+-- Q3
+select
+  EmployeeName
+, EMail
+, Height
+from
+  Employees
+;
+
+-- Q4
+select
+  CustomerCode
+, CustomerName
+, CustomerCode
+from
+  Customers
+;
+
+-- Q5
+select
+  ProductCode
+, Price
+, ProductName
+, ProductCode
+from
+  Products
+;
