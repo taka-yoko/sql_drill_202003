273427c91804897e40caa694e892dbaff4a8b678
taka-yoko <*****@users.noreply.github.com>
2020-03-16 23:41:35 +0900

    finished ch2_1 and started ch2_2

---
+++ ch2_1.sql
@@ -0,0 +1,41 @@
+-- ex1
+select
+  CustomerName
+from
+  Customers
+;
+
+-- Q1
+select
+  Address
+from
+  Customers
+;
+
+-- Q2
+select
+  ProductName
+from
+  Products
+;
+
+-- Q3
+select
+  Price
+from
+  Products
+;
+
+-- Q4
+select
+  EmployeeName
+from
+  Employees
+;
+
+-- Q5
+select
+  EMail
+from
+  Employees
+;
