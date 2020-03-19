9ac947284a9d22c6e81a972de98d465243366f15
taka-yoko <*****@users.noreply.github.com>
2020-03-20 01:31:08 +0900

    finished ch2_16

---
+++ ch2_16.sql
@@ -0,0 +1,50 @@
+-- ch2_16.sql
+
+-- ex
+select distinct
+  Address as "住所"
+from
+  Customers
+;
+
+-- Q1
+select distinct
+  HireFiscalYear
+from
+  Employees
+;
+
+-- Q2
+select distinct
+  CustomerID
+, ProductID
+from
+  Sales
+;
+
+-- Q3
+select distinct
+  CustomerClassID
+, PrefecturalID
+from
+  Customers
+;
+
+-- Q4
+select distinct
+  CustomerID
+, ProductID
+, EmployeeID
+from
+  Sales
+order by
+  CustomerID, ProductID
+;
+
+-- Q5
+select distinct
+  Price
+, CategoryID
+from
+  Products
+;
